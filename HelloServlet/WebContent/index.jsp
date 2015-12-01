b0a067f70cf417978c55b3b4fa615b40aa39ab2d
Saurabh Khare <saurabh.khare1000@gmail.com>
2015-12-01 23:49:25 +0530

    add HelloServlet JavaEE project

---
+++ HelloServlet/WebContent/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Landing Page JSP</title>
+</head>
+<body>
+	<form action="HelloServletPath" method="get">
+		<h3>Enter your name</h3>
+		<input type="text" name="name" size="40px"> <input
+			type="submit" value="submit">
+	</form>
+</body>
+</html>
---
+++ HelloServlet/src/com/servletdemo/HelloServlet.java
@@ -0,0 +1,37 @@
+package com.servletdemo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet(description = "Simple Servlet that gets a parameter and sends an HTTP/HTML response", urlPatterns = { "/HelloServletPath" })
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		String name = request.getParameter("name");
+		request.setAttribute("name", name); //This will be available as ${name}
+		request.getRequestDispatcher("greeting.jsp").forward(request, response);
+	}
+
+}
